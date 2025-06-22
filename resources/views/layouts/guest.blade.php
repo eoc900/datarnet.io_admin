5863bad59323b01cbdd627dd4d4f7c507d6016a6
Eoc900 <Eoc900@gmail.com>
2025-06-22 17:46:03 -0600

    cambiamos en archivos app y guest.blade para no usar vite

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,8 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-          @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="{{ asset('build/assets/app-C-h92KH8.css') }}">
+        <script src="{{ asset('build/assets/app-twnkEdqc.js') }}" defer></script>
 
     </head>
     <body class="font-sans text-gray-900 antialiased">
