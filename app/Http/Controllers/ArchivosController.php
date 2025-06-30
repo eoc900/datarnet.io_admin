da722b42f48929832ef3aec9e42f4697de1e79d8
Eoc900 <Eoc900@gmail.com>
2025-06-29 23:14:17 -0600

    edicion de informes con editar_archivo

--- app/Http/Controllers/ArchivosController.php
+++ app/Http/Controllers/ArchivosController.php
@@ -193,10 +193,12 @@ class ArchivosController extends Controller
             'informe' => 'informes'
         ];
 
+        return response()->json(["ruta"=>$tipo]);
         // Verificamos que el tipo sea válido
         if (!array_key_exists($tipo, $rutas)) {
             abort(404, "Tipo de archivo no válido");
         }
+
 
         // Construimos la ruta del archivo
         $ruta = $rutas[$tipo] . '/' . $nombreArchivo . '.json';
